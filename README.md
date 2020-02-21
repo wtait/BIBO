22e8223fc02c6fad2d6e3659bee87bb5472854f7
jameshih <j@shih.app>
2020-02-21 00:26:48 -0500

    setup dev env in docker-compose

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # BIBO
 BIBO network using MTP on Ethereum
+
+using docker-compose 
+```
+docker-compose build 
+docker-compose up 
+docker-compose exec truffle truffle test
+
+```
--- test/proxy.test.js
+++ test/proxy.test.js
@@ -11,48 +11,51 @@ require("chai")
   .use(require("chai-as-promised"))
   .should();
 
-  describe('Proxy accounts', () => {
+describe("Proxy accounts", () => {
+  const MTP = contract.fromArtifact("MTP");
+  const NFTContract = contract.fromArtifact("TestNFT");
+  const [alice, bob] = accounts;
+  const ProxyFactory = contract.fromArtifact("AuthereumProxyFactory");
+  const ProxyAccount = contract.fromArtifact("AuthereumProxy");
 
-    const MTP = contract.fromArtifact('MTP');
-    const NFTContract = contract.fromArtifact('TestNFT');
-    const[alice, bob] = accounts;
-    const ProxyFactory = contract.fromArtifact('AuthereumProxyFactory');
-    const ProxyAccount = contract.fromArtifact('AuthereumProxy');
+  beforeEach(async function() {
+    this.mtp = await MTP.new();
+    this.mtpAddress = await this.mtp.address;
+    this.nftokenContract = await NFTContract.new();
+    this.nfTokenAddress = await this.nftokenContract.address;
+    this.nftokenId = new BN("5042");
+    this.nft = await this.nftokenContract.mintUniqueTokenTo(
+      alice,
+      this.nftokenId
+    );
+    await this.mtp.depositToken(this.nfTokenAddress, alice, this.nftokenId);
+    this.proxyFactory = await ProxyFactory.new(this.mtpAddress);
+    this.proxyAccount = await ProxyAccount.new(this.proxyFactory.address);
+    // console.log(
+    //     "mtp address: " + this.mtpAddress + '\n' +
+    //     "proxy Factory address: " + this.proxyFactory.address + '\n' +
+    //     'proxy account address: ' + this.proxyAccount.address
+    //);
+  });
 
-
-    beforeEach(async function() {
-        this.mtp = await MTP.new();
-        this.mtpAddress = await this.mtp.address;
-        this.nftokenContract = await NFTContract.new();
-        this.nfTokenAddress = await this.nftokenContract.address;
-        this.nftokenId = new BN('5042');
-        this.nft = await this.nftokenContract.mintUniqueTokenTo(alice, this.nftokenId);
-        await this.mtp.depositToken(this.nfTokenAddress, alice, this.nftokenId);
-        this.proxyFactory = await ProxyFactory.new(this.mtpAddress);
-        this.proxyAccount = await ProxyAccount.new(this.proxyFactory.address);
-        // console.log(
-        //     "mtp address: " + this.mtpAddress + '\n' + 
-        //     "proxy Factory address: " + this.proxyFactory.address + '\n' +
-        //     'proxy account address: ' + this.proxyAccount.address
-        //);
+  describe("Proxy Accounts", function() {
+    it("should deploy a proxy account", async function() {
+      //console.log(this.mtpAddress);
+      let ProxyAccountAddress = this.proxyAccount.address;
+      ProxyAccountAddress.should.be.an("string").that.includes("0x");
     });
-
-    describe("Proxy Accounts", function() {
-
-        it("should deploy a proxy account", async function() {
-            //console.log(this.mtpAddress);
-            let ProxyAccountAddress = this.proxyAccount.address
-            ProxyAccountAddress.should.be.an('string').that.includes('0x');
-        });
-        it('should point to mtp for implementation', async function() {
-            
-            let implementationAddress = await this.proxyAccount.implementation.call();
-            implementationAddress.should.equal(this.proxyFactory.address);
-        });
-        it('should return address of new proxy', async function() {
-           let newProxyAddress = await this.proxyFactory.createProxy.call(1,'test', []);
-           console.log(newProxyAddress);
-        });
+    it("should point to mtp for implementation", async function() {
+      let implementationAddress = await this.proxyAccount.implementation.call();
+      implementationAddress.should.equal(this.proxyFactory.address);
     });
-
+    it("should return address of new proxy", async function() {
+      let newProxyAddress = await this.proxyFactory.createProxy.call(
+        1,
+        "test",
+        []
+      );
+      console.log(newProxyAddress);
+    });
+  });
 });
+
